934441301624370c2d2318e847f071838ab9283c
gakitomo <tmo8002@gmail.com>
2021-06-12 14:02:31 +0900

    Update models.py

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
 # Create your models here.
