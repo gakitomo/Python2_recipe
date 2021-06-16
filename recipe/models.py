39428cc46e6f9ce22235107b8082ba6fb30b2690
gakitomo <tmo8002@gmail.com>
2021-06-16 19:31:33 +0900

    Update models.py

--- recipe/models.py
+++ recipe/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    title = models.CharField(verbose_name="タイトル", max_length=200)
+    content = models.TextField(verbose_name="内容")
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = "レシピ"
+        verbose_name_plural = "レシピ"
+    
+    def __str__(self):
+        return self.title
 # Create your models here.
