d7abc7383149cac33be21bd6979500bef0dab644
gakitomo <tmo8002@gmail.com>
2021-11-13 11:28:28 +0900

    Create recipe_detail.html

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from lib.views import IndexTemplateView
-from recipe.views import RecipeListView, RecipeCreateView
+from recipe.views import RecipeListView, RecipeCreateView, RecipeDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipe/', RecipeListView.as_view(), name="recipe-index"),
     path('recipe/create', RecipeCreateView.as_view(), name="recipe-create"),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe-detail"),
     path('', IndexTemplateView.as_view(), name="index"),
 ]
