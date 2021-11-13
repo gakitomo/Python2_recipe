305a544fe8aa656b0ccb53ae765ddc50e851ee4f
gakitomo <tmo8002@gmail.com>
2021-11-13 15:41:05 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from lib.views import IndexTemplateView
-from recipe.views import (
-    RecipeListView, RecipeCreateView, RecipeDetailView, RecipeUpdateView, RecipeDeleteView)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recipe/', RecipeListView.as_view(), name="recipe-index"),
-    path('recipe/create', RecipeCreateView.as_view(), name="recipe-create"),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe-detail"),
-    path('recipe/<int:pk>/update',RecipeUpdateView.as_view(), name="recipe-update"),
-    path('recipe/<int:pk>/delete',RecipeDeleteView.as_view(), name="recipe-delete"),
+    path('recipe/', include("recipe.urls")),
     path('', IndexTemplateView.as_view(), name="index"),
 ]
