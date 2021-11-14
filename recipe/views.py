dff36e8513282ceaddf89b87ad878d4810f51d66
gakitomo <tmo8002@gmail.com>
2021-11-14 22:54:49 +0900

    Update base.html

--- recipe/views.py
+++ recipe/views.py
@@ -14,6 +14,14 @@ class RecipeCreateView(CreateView):
   fields = ["title", "content", "description"]
   success_url = reverse_lazy("recipe:index")
 
+  def form_valid(self, form):
+    messages.success(self.request, "保存しました")
+    return super().form_valid(form)
+
+  def form_invalid(self, form):
+    messages.error(self.request, "保存できませんでした")
+    return super().form_invalid(form)
+
 class RecipeDetailView(DetailView):
   model = Recipe
 
