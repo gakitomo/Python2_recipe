a7fe10a15d6c91e4ba61b0085d79a68b0fa67f62
gakitomo <tmo8002@gmail.com>
2021-11-14 22:15:12 +0900

    Update views.py

--- recipe/views.py
+++ recipe/views.py
@@ -2,13 +2,15 @@ from django.views.generic import(ListView, CreateView, DetailView, UpdateView, D
 
 from .models import Recipe
 
+from django.urls import reverse, reverse_lazy
+
 class RecipeListView(ListView):
   model = Recipe
 
 class RecipeCreateView(CreateView):
   model = Recipe
   fields = ["title", "content", "description"]
-  success_url = "/"
+  success_url = reverse_lazy("recipe:index")
 
 class RecipeDetailView(DetailView):
   model = Recipe
@@ -20,4 +22,8 @@ class RecipeUpdateView(UpdateView):
 
 class RecipeDeleteView(DeleteView):
   model = Recipe
-  success_url = "/"
+  success_url = reverse_lazy("recipe:index")
+
+def get_success_url(self):
+  pk = self.kwargs.get("pk")
+  return reverse("recipe:detail", kwargs={"pk":pk})
