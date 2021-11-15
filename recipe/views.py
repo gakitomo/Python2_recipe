c829d28d60095f5bcd136d9d6b7d8ac29667eb1f
gakitomo <tmo8002@gmail.com>
2021-11-15 22:15:02 +0900

    Update views.py

--- recipe/views.py
+++ recipe/views.py
@@ -30,6 +30,18 @@ class RecipeUpdateView(UpdateView):
   fields = ["title", "content", "description"]
   success_url = "/"
 
+  def get_success_url(self):
+    pk = self.kwargs.get("pk")
+    return reverse("recipe:detail", kwargs = {"pk":pk})
+
+  def form_valid(self, form):
+    messages.success(self.request, "更新しました")
+    return super().form_valid(form)
+
+  def form_invalid(self, form):
+    messages.error(self.request, "更新できませんでした")
+    return super().form_invalid(form)
+
 class RecipeDeleteView(DeleteView):
   model = Recipe
   success_url = reverse_lazy("recipe:index")
