709f52652a87ef1801f5ca039d6ecb74e2714a6f
gakitomo <tmo8002@gmail.com>
2021-11-21 18:48:50 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from lib.views import IndexTemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recipe/', include("recipe.urls")),
+    path('recipe/', include("recipe.urls", namespace="recipe")),
     path('', IndexTemplateView.as_view(), name="index"),
 ]
+
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
