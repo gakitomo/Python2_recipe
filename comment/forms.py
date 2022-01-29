599757efe9ad05431700d84859bb12b8bd8ca333
gakitomo <tmo8002@gmail.com>
2022-01-29 16:27:14 +0900

    Update forms.py

--- comment/forms.py
+++ comment/forms.py
@@ -7,3 +7,9 @@ class CommentForm(forms.ModelForm):
   class Meta:
     model = Comment
     fields = ["content", "recipe", ]
+    widgets = {
+      "recipe":forms.HiddenInput()
+    }
+    labels = {
+      "content":"コメントをどうぞ"
+    }
