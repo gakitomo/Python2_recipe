f7ff43647ca21d1134a4eca570e7f22b17c32a29
gakitomo <tmo8002@gmail.com>
2022-01-29 11:47:54 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'recipe',
+    'comment',
+    
     'crispy_forms',
     'imagekit',
 ]
