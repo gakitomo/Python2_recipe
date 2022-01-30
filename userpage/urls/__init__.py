415ebe1f033e6a158df6d584ed7086fd683ecbcc
gakitomo <tmo8002@gmail.com>
2022-01-30 18:50:41 +0900

    Update __init__.py

--- userpage/urls/__init__.py
+++ userpage/urls/__init__.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import UserpageTemplateView 
+from ..views import UserpageTemplateView 
 
 app_name = "userpage"
 
 urlpatterns = [
-  path("", UserpageTemplateView.as_view(), name="index"), 
+  path("", UserpageTemplateView.as_view(), name="index"),
+  path("recipe/", include("userpage.urls.recipe", namespace="recipe")),
 ]
