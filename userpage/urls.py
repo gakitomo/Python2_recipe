c837218c24094d78bb479cdbec7feecbe344941b
gakitomo <tmo8002@gmail.com>
2022-01-30 18:10:16 +0900

    Create Userpage

--- userpage/urls.py
+++ userpage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from .views import UserpageTemplateView 
+
 app_name = "userpage"
 
 urlpatterns = [
-  
+  path("", UserpageTemplateView.as_view(), name="index"), 
 ]
--- userpage/views.py
+++ userpage/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from recipe.models import Recipe
+
+class UserpageTemplateView(TemplateView):
+  template_name = "userpage/index.html"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+
+    if self.request.user.is_authenticated:
+      context['recipe_list'] = Recipe.objects.filter(user=self.request.user)
+    else:
+      context['recipe_list'] = None
+
+    return context
