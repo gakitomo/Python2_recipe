630c4f3b393b66b1f9c0bd3775ec9751da52c03a
gakitomo <tmo8002@gmail.com>
2022-02-01 22:09:02 +0900

    Create logout.html

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -12,6 +12,7 @@ urlpatterns = [
     path('comment/', include("comment.urls", namespace = "comment")),
     path('userpage/',include("userpage.urls", namespace = "userpage")),
     path('login', LoginView.as_view(template_name = "login.html"), name = "login"),
+    path('logout', LogoutView.as_view(template_name = "logout.html"), name = "logout"),
 
     path('', IndexTemplateView.as_view(), name = "index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
