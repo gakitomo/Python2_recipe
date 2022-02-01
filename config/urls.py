8e7102dd8312f5082fd3ed968ed4c10832ba7830
gakitomo <tmo8002@gmail.com>
2022-02-01 20:36:31 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,6 +11,7 @@ urlpatterns = [
     path('recipe/', include("recipe.urls", namespace = "recipe")),
     path('comment/', include("comment.urls", namespace = "comment")),
     path('userpage/',include("userpage.urls", namespace = "userpage")),
+    path('login', LoginView.as_view(template_name = "login.html"), name = "login"),
 
     path('', IndexTemplateView.as_view(), name = "index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
