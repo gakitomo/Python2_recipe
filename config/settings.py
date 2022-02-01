518259f0740b9864b12844cb552e9794f87470dd
gakitomo <tmo8002@gmail.com>
2022-02-01 20:38:35 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,3 +143,5 @@ MEDIA_ROOT = BASE_DIR / Path('media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = reverse_lazy("userpage:index")
